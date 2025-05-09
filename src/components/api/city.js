4921e850cb55d195b231fe08455e26ec075f394e
Abir-Ashab <bsse1315@iit.du.ac.bd>
2025-05-09 11:44:10 +0600

    update city code

--- src/components/api/city.js
+++ src/components/api/city.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 19f58f443f3d411d3c16fbe73e656b7ac3b9ddfc
-const API_KEY = "1a19e7763c77142fd6b2b5cab6b6ec98"; 
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-
-export async function fetchWeatherData(city) {
-<<<<<<< HEAD
-=======
 export async function fetchWeatherData(city) {
     const API_KEY = "1a19e7763c77142fd6b2b5cab6b6ec98"
     const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
->>>>>>> 21a8d76 (update content and structure of the app)
-=======
->>>>>>> 19f58f443f3d411d3c16fbe73e656b7ac3b9ddfc
     try {
         const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         if (!response.ok) {
